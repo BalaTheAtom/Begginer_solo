35695bbd2cc1198653c311a4ffd04ac25f05da77
BalaTheAtom <addagatlabalaswamy@hotmail.com>
2017-09-21 01:16:00 +0530

    1.1

--- class_practice.py
+++ class_practice.py
@@ -4,15 +4,20 @@ class Person:
         self.firstname = first
         self.lastname = last
 
+
     def Name(self):
         return self.firstname + " " + self.lastname
 
 class Employee(Person):
     def __init__(self,first,last,pay):
+        Person.__init__(self,first,last)
+        self.pay="     "+str(pay)
 
-        self.pay=pay
-        return '{} {} and his pay is {}'.format(Person.first,Person.last,pay)
+    def desc(self):
+        return self.Name()+(self.pay)
 p=Person("Balaswamy","Addagatla")
-# e=Employee("raj","Krishna",50000)
+e=Employee("raj","Krishna",50000)
+f=Employee("Siva","Satya",50000)
 
 print(p.Name())
+print(f.desc())
